c2206f0e8900eeafe434a5fa9d5e119fa11ebaaf
ThiagoDuutra <thiagoduutra01@gmail.com>
2023-04-06 09:03:21 -0300

    feat#4: Cria sobre mim

--- src/Pages/AboutMe/AboutMe.jsx
+++ src/Pages/AboutMe/AboutMe.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
+import "./AboutMe.scss";
+
+import Photo from "../../img/thiago.png";
+
 const AboutMe = () => {
-  return <div>AboutMe</div>;
+  return (
+    <div className="container animeLeft">
+      <div className="container-aboutMe">
+        <div className="aboutMe">
+          <h2 className="title-aboutMe">Sobre mim</h2>
+          <p>
+            Olá, me chamo Thiago, tenho 25 anos. Sou desenvolvedor Front End.
+            Curso Sistemas de Informações no Cefet-RJ, atualmente estou no 6
+            período. Estudando há cerca de 8 meses focado em HTML, CSS,
+            JavaScript, SASS e iniciando projetos pessoais com React.<br></br>
+            <br></br>
+            Amo me desafiar e resolver problemas. Sempre disposto a aprender
+            novas tecnologias, novos desafios e principalmente aprimorar meus
+            conhecimentos.
+          </p>
+        </div>
+        <div className="img-aboutMe">
+          <div className="wrapper-img">
+            <img className="img" src={Photo} alt="foto de perfil" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AboutMe;
